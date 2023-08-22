cfd9049d3349e8f63bfbc9b14914e285c2622544
SavAlexV <aleksejsavisev223@gmail.com>
2023-08-22 12:59:16 +0300

    first commit

---
+++ home work10/lesson4/firstdj/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from .models import Advertisement
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'image_adm','auction', 'updated_date',]
+    list_filter = ['created_at', 'auction']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+
+        (
+
+            "Общее", {
+
+                "fields": ("title", "description", "user", "image"),
+
+            }
+
+        ),
+
+        (
+
+            "Финансы", {
+
+                "fields": ("price", "auction")
+
+            }
+
+        )
+
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, gueryset):
+        gueryset.update(auction=False)
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, gueryset):
+        gueryset.update(auction=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
